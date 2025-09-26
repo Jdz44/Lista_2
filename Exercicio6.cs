4a475d688c467ced6e4ee10414f0ea5abbdacf26
JOAO EDUARDO ALVES SILVA <10617296642@CAMPUS.MACHADO.MCH>
2025-09-26 16:29:23 -0300

    Lista_2

---
+++ Exercicio6.cs
@@ -0,0 +1,15 @@
+using System;
+using MinhaBiblioteca;
+
+class Exercicio6
+{
+    static int [,] somarMatriz(int[,] matrizA, int[,] matrizB)
+    {
+        if(matrizA.GetLength(0) != matriz.GetLength(0) ||
+        matrizA.GetLength(1) != matriz.GetLength(1))
+        {
+            Console.WriteLine("As matrizes não tem a mesma ordem!");
+            return null;
+        }
+    }
+}
